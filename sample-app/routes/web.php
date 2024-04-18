e0c4c8ba399e5298f5e4e9dc4815262cae98c748
TAKUO2000 <162293482+TAKUO2000@users.noreply.github.com>
2024-04-18 17:33:31 +0900

    BookControllerの追加とルーティングの追加・整理

--- sample-app/routes/web.php
+++ sample-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\BookController;
 use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,19 @@ Route::get('/', function () {
 Route::get('messages' , [MessageController::class,'index']);
 // locaklhost/messagesからのPOSTをMessageControllerのstoreメソッドが対応
 Route::post('messages',[MessageController::class,'store']);
+
+
+// Route::get('admin/books',[BookController::class,'index'])
+//     ->name('book.index');
+// Route::get('admin/books/{id}',[BookController::class,'show'])
+//     ->whereNumber('id')
+//     ->name('book.show');
+
+Route::prefix('admin/books')
+    ->name('book.')
+    ->controller(BookController::class)
+    ->group(function(){
+    Route::get('','index')->name('index');
+    Route::get('{id}','show')->whereNumber('id')->name('show');
+    });
 
