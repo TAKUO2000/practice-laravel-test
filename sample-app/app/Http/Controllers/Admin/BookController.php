1a31865470f4d477740f7016cb757365cfe22e1d
TAKUO2000 <162293482+TAKUO2000@users.noreply.github.com>
2024-04-18 18:28:40 +0900

    書籍登録ページadmin/books/createの追加

--- sample-app/app/Http/Controllers/Admin/BookController.php
+++ sample-app/app/Http/Controllers/Admin/BookController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class BookController extends Controller
 {
@@ -18,9 +20,33 @@ class BookController extends Controller
         return $books;
     }
 
-    public function show(string $id)// Book <-?
+    public function show(string $id)
     {
         $book = Book::findOrFail($id);
         return $book;
     }
+
+    public function create():View
+    {
+        $categories = Category::all();
+
+        return view('admin/book/create',[
+            'categories' => $categories
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $book = new Book();
+
+        $book->category_id = $request->category_id;
+        $book->title = $request->title;
+        $book->price = $request->price;
+
+        $book->save();
+
+        return $book;
+    }
 }
+
+
